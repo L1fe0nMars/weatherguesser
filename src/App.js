f98c07d634b6458dda2c6257096c23aeb3b64504
L1fe0nMars <jkama005@gmail.com>
2023-11-12 13:49:14 -0500

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=045d974fe8c45d8b0e0bb4c31d908098&units=imperial';
+const json = require('./city-list.json');
+console.log(json.length);
+
+const App = () => {
+    const getWeather = async (cityID) => {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        
+        console.log(data);
+    }
+
+    return (
+        <div className="App">
+            <button onClick={() => getWeather()}>Weather</button>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/WeatherGame.jsx
@@ -0,0 +1,11 @@
+
+
+const WeatherGame = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default WeatherGame;
