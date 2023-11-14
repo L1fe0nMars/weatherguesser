e38dda78c11dc45de4b47f7471cf54bad813b24f
L1fe0nMars <jkama005@gmail.com>
2023-11-14 01:30:01 -0500

    Convert country and state codes to names

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,18 +1,26 @@
+import { countryCodeToName, stateCodeToName } from "../Utils";
+
 const WeatherCard = (props) => {
     const city = props.city;
+    const country = countryCodeToName[city.sys.country];
     const temp = Math.round(city.main.temp);
-    const description = city.weather[0].description;
-    const desc = description.charAt(0).toUpperCase() + description.slice(1);
+    const desc = city.weather[0].description;
+    const description = desc.charAt(0).toUpperCase() + desc.slice(1);
+    let state = props.city.state;
+
+    if (stateCodeToName[state]) {
+        state = stateCodeToName[state];
+    }
 
     return (
         <div>
             {
                 city.state.length > 0
                 ? (
-                    <h1>{`${city.name}, ${city.state}, ${city.sys.country}`}</h1>
+                    <h1>{`${city.name}, ${state}, ${country}`}</h1>
                 )
                 : (
-                    <h1>{`${city.name}, ${city.sys.country}`}</h1>
+                    <h1>{`${city.name}, ${country}`}</h1>
                 )
             }
             
@@ -27,7 +35,7 @@ const WeatherCard = (props) => {
             }
 
             <img src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`} alt="Weather icon" />
-            <p>{desc}</p>
+            <p>{description}</p>
             <p>{`Humidity: ${city.main.humidity}%`}</p>
         </div>
     );
