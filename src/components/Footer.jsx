b99a61df54c8da84fed4dcbb174edaf6dae2ef23
L1fe0nMars <jkama005@gmail.com>
2023-11-14 01:30:19 -0500

    Added footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-
+            <p>Weather data provided by <a href="https://openweathermap.org/" target="_blank" rel="noopener noreferrer">OpenWeatherMap API</a>.</p>
         </footer>
     );
 }
