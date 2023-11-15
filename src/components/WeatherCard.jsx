e48ce0bae65ab676616451dd6e4610499161837f
L1fe0nMars <jkama005@gmail.com>
2023-11-15 14:14:49 -0500

    Added local time

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,6 +2,18 @@ import { useContext } from 'react';
 import { countryCodeToName, stateCodeToName } from "../Utils";
 import { SaveDataContext } from '../context/SaveDataContext';
 
+const getLocalTime = (city, timeFormat) => {
+    const unixTimestamp = city.dt;
+    const timezoneOffset = city.timezone;
+    const timestampMillis = unixTimestamp * 1000;
+    const date = new Date(timestampMillis);
+    const localTime = new Date(date.getTime() + timezoneOffset * 1000);
+    const setTimeFormat = timeFormat === '12hr' ? true : false;
+    const formattedLocalTime = localTime.toLocaleTimeString([], { timeZone: 'GMT', hour12: setTimeFormat, timeStyle: 'short' });
+
+    return formattedLocalTime;
+}
+
 const WeatherCard = (props) => {
     const { data } = useContext(SaveDataContext);
 
@@ -41,6 +53,7 @@ const WeatherCard = (props) => {
             <img src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`} alt="Weather icon" />
             <p>{description}</p>
             <p>{`Humidity: ${city.main.humidity}%`}</p>
+            <p>{`Local Time: ${getLocalTime(city, data.timeFormat)}`}</p>
         </div>
     );
 }
